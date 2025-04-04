5f4b1e8a281e53c9a4d1983256c1c9d02d255863
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-04-05 01:14:02 +0530

    updates

---
+++ code/variables.c
@@ -0,0 +1,13 @@
+# include  <stdio.h>
+
+int main()
+{
+    int age = 14;
+    float height = 5.9;
+    char grade = 'A';
+    printf("Age: %d\n", age);
+    printf("Height: %f\n", height);
+    printf("grade: %c\n", &grade);
+    getchar();
+    return 0;
+}
