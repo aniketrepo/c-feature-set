3b45b3ca10aa7dea1423ca4b58a7d5bf563bc8c8
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-04-05 01:15:52 +0530

    updates

--- code/variables.c
+++ code/variables.c
@@ -5,6 +5,7 @@ int main()
     int age = 14;
     float height = 5.9;
     char grade = 'A';
+    
     printf("Age: %d\n", age);
     printf("Height: %f\n", height);
     printf("grade: %c\n", &grade);
