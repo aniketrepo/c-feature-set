9f4cd432479ed995fd69f5ecd888815691d83e16
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-04-07 05:10:54 +0530

    updates

---
+++ code/formatspecifiers.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main()
+{
+    // Format specifiers
+    int i = 0;
+    float f = 0.0;
+    char c = 'a';
+    char str[100] = "Hello World!";
+
+    // Printing them
+    printf("Integer: %d\n", i);
+    printf("Float: %f\n", f);
+    printf("Character: %c\n", c);
+    printf("String: %s\n", str);
+    getchar();
+    return 0;
+}
