100bd27d0b63fbd9eb3a3cff736e48c7221d4206
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-04-07 05:21:59 +0530

    updates

--- code/formatspecifiers.c
+++ code/formatspecifiers.c
@@ -2,13 +2,11 @@
 
 int main()
 {
-    // Format specifiers
     int i = 0;
     float f = 0.0;
     char c = 'a';
     char str[100] = "Hello World!";
 
-    // Printing them
     printf("Integer: %d\n", i);
     printf("Float: %f\n", f);
     printf("Character: %c\n", c);
