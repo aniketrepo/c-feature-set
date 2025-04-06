eef6ff71284226704aab8f65e20bb79fca3fb075
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-04-07 05:25:54 +0530

    updates

---
+++ code/formatspecifiers3.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+
+int main()
+{
+    printf("Hello, my name is %s, I am %d years old, my height is %f and my grade is %c.\n", "Aniket", 19, 5.9, 'A');
+    getchar();
+    return 0;
+}
