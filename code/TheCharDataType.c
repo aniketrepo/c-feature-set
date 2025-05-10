6f753a60bf71497d1f569c3058cd68b1c092dc9e
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-05-10 15:35:59 +0530

    updates

---
+++ code/TheCharDataType.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main()
+{
+    // Initializing Variables with char data type
+    char myGrade = 'A';
+    char a = 'Z', b = 'Y', c = 'X';
+
+    // Printing variables
+    printf("%c\n", myGrade);
+    printf("%c%c%c\n", a, b, c);
+
+    return 0;
+}   
