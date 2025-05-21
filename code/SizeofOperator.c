013cbdaa48925f75c1a5a13ed425bbb61aa5dd4b
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-05-21 15:01:46 +0530

    updates

--- code/SizeofOperator.c
+++ code/SizeofOperator.c
@@ -5,9 +5,9 @@ int main(){
     double b = 3.14;
     char c = 'A';
 
-    printf("Size of int: %d bytes\n", sizeof(a));
-    printf("Size of float: %d bytes\n", sizeof(b));
-    printf("Size of char: %d bytes\n", sizeof(c));
+    printf("Size of int: %lu bytes\n", sizeof(a));
+    printf("Size of float: %lu bytes\n", sizeof(b));
+    printf("Size of char: %lu bytes\n", sizeof(c));
 
     return 0;
 }
