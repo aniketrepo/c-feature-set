862bb4694948ec232df19e4c2cff122f2d75c95a
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-06-14 23:20:13 +0530

    updates

---
+++ code/ImplicitTypeConversion.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main()
+{
+    // compiler automatically converts the value of one type to another
+    float myFloat = 9; // giving an int value to float
+
+    printf("%f\n", myFloat);
+
+    return 0;
+}
