321baffe1e77545be40a0be556414021658ec18a
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-06-14 23:27:03 +0530

    updates

--- code/ImplicitTypeConversion.c
+++ code/ImplicitTypeConversion.c
@@ -3,9 +3,14 @@
 int main()
 {
     // compiler automatically converts the value of one type to another
+    
     float myFloat = 9; // giving an int value to float
+    int myInt = 9.99; // giving a float value to int
+    float mySum = 5/2; 
 
     printf("%f\n", myFloat);
+    printf("%d\n", myInt);
+    printf("%f\n", mySum);
 
     return 0;
 }
