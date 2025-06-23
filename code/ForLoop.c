a59ef71b90e8563491d4ba8b79927110cd2bd22d
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-06-24 05:17:02 +0530

    ForLoop.c

---
+++ code/ForLoop.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main()
+{
+    int i;
+
+    // (i=0) is executed one time before execution of code block
+    // (i<=5) is the condition for executing the code block
+    // (i++) is executed every time after the code is executed.
+    for (i = 0; i <= 5; i++){
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
