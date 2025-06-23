ef09b210ecb5fa0b854c8d255505d98e4a73285d
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-06-24 05:24:23 +0530

    ForLoop.c

--- code/ForLoop.c
+++ code/ForLoop.c
@@ -6,7 +6,7 @@ int main()
 
     // (i=0) is executed one time before execution of code block
     // (i<=5) is the condition for executing the code block
-    // (i++) is executed every time after the code is executed.
+    // (i++) is executed every time after the code is executed
     for (i = 0; i <= 5; i++){
         printf("%d\n", i);
     }
