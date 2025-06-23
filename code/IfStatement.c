9f123aa5079199daa1579dcb7980ac9b2a8a1e9a
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-06-24 03:27:48 +0530

    IfStatement.c

---
+++ code/IfStatement.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main()
+{
+    if (20<80)
+    {
+        printf("20 is greater than 80\n\n");
+    }
+
+    int x = 20;
+    int y = 18;
+    if (x>y)
+    {
+        printf("x is greater than y\n\n");
+    }
+
+    return 0;
+}
