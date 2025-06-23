b8fc2377640caded23d3da44c50ea77da6aa2ea5
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-06-24 04:25:28 +0530

    DoorCode.c

---
+++ code/DoorCode.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main()
+{
+    int doorCode = 1993;
+
+    if (doorCode == 1993){
+        printf("Code is correct, you may enter.\n");
+    }
+    else{
+        printf("Code is incorrect, you may not enter.\n");
+    }
+
+    return 0;
+}
