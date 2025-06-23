22e5c69757bac63c03d95644c298753f43b2fdbb
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-06-24 04:08:04 +0530

    ElseStatement.c

---
+++ code/ElseStatement.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main()
+{
+    int time = 20;
+    
+    if (time < 18)
+    {
+        printf("Good Day\n");
+    }
+    else
+    {
+        printf("Good Evening\n");
+    }
+
+    return 0;
+}
