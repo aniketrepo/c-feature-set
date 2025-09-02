0f28ae5dc486b8a25fcca00eb04e7ade678bf11e
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-09-02 12:08:49 +0530

    Update ArraySize.c

--- code/ArraySize.c
+++ code/ArraySize.c
@@ -4,10 +4,13 @@ int main()
 {  
     int myNum[5];
     
-    myNumbers[0] = 25;
-    myNumbers[1] = 50;
-    myNumbers[2] = 75;
-    myNumbers[3] = 100;
+    myNum[0] = 25;
+    myNum[1] = 50;
+    myNum[2] = 75;
+    myNum[3] = 100;
+
+    printf("%zu\n", sizeof(myNum));   // will return 20 as each int value takes 4 bytes of space, and there are 50 such elements
+    printf("%zu\n", sizeof(myNum) / sizeof(myNum[0]));  // will return the number of elements in an array by dividing the actual size with the storage taken by the first element in the array
 
     return 0;
 }
