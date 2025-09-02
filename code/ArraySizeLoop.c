f8cb2c6704acb571dae88b6587a9be6f7f7c7e67
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-09-02 12:21:13 +0530

    Update ArraySizeLoop.c

--- code/ArraySizeLoop.c
+++ code/ArraySizeLoop.c
@@ -3,7 +3,7 @@
 int main(){
 
     int myNum[] = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100};
-    int length = sizeof(myNum) / myNum[0];
+    int length = sizeof(myNum) / sizeof(myNum[0]);
     int i;
 
     for (i = 0; i < length; i++){
